da0a3b1a3848ca6180c8c06a0fca17996c5824b1
rPlaceOhio <102863614+rPlaceOhio@users.noreply.github.com>
2022-04-02 10:12:04 -0400

    Create TMScript.js

---
+++ TMScript.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         rPlaceOhio (410, 273)
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  try to take over the canvas!
+// @author       oralekin
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://github.com/rPlaceOhio/rPlaceOhio/blob/main/images/rPlaceOhio%20BlockO%20Overlay%20410%20273.png?raw=true";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
