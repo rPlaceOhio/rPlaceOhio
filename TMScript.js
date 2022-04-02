1a84b9f25e6409e2a50be219940af17a333ac9f5
rPlaceOhio <102863614+rPlaceOhio@users.noreply.github.com>
2022-04-02 10:42:00 -0400

    Update TMScript.js

--- TMScript.js
+++ TMScript.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://github.com/rPlaceOhio/rPlaceOhio/blob/main/images/rPlaceOhio%20BlockO%20Overlay%20410%20273.png?raw=true";
+            i.src = "https://github.com/rPlaceOhio/rPlaceOhio/blob/main/images/rPlaceOhio%20BlockO%20Overlay.png?raw=true";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
