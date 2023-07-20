1e36421d0ae3d6d714c02bd8dbe02026870f7bfb
rPlaceOhio <102863614+rPlaceOhio@users.noreply.github.com>
2023-07-20 18:52:24 -0400

    Update TMScript.js

--- TMScript.js
+++ TMScript.js
@@ -1,24 +1,32 @@
 // ==UserScript==
-// @name         rPlaceOhio (410, 273)
+// @name         rPlaceOhio (106, 150)
 // @namespace    http://tampermonkey.net/
 // @version      0.3
-// @description  try to take over the canvas!
-// @author       oralekin
-// @match        https://hot-potato.reddit.com/embed*
+// @description  Ohioans vs. r/Place
+// @author       rPlaceOhio
+// @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
 // ==/UserScript==
+
+
+const overlay = "https://github.com/rPlaceOhio/rPlaceOhio/blob/main/images/rPlaceOhio%20BlockO%20Overlay.png?raw=true" + Math.floor(Date.now() / 10000);
+console.log("[PLACE] Placing overlay for Steins;Place:", overlay);
+
 if (window.top !== window.self) {
+    const overlay_img = document.createElement("img");
+    overlay_img.src = overlay;
+    overlay_img.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+
     window.addEventListener('load', () => {
-            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
-        (function () {
-            const i = document.createElement("img");
-            i.src = "https://github.com/rPlaceOhio/rPlaceOhio/blob/main/images/rPlaceOhio%20BlockO%20Overlay.png?raw=true";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
-            console.log(i);
-            return i;
-        })())
+        const canvas = document.getElementsByTagName('garlic-bread-embed')[0]
+            .shadowRoot.children[0]
+            .getElementsByTagName('garlic-bread-share-container')[0]
+            .getElementsByTagName('garlic-bread-camera')[0]
+            .getElementsByTagName('garlic-bread-canvas')[0]
+            .shadowRoot.children[0];
 
+        console.log("[PLACE] Placing on canvas:", canvas);
+        canvas.appendChild(overlay_img);
     }, false);
-
 }
